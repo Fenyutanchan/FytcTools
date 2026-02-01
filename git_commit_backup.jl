9a4a8e426e125b3bd360c13e93e45cc97382ebde
Fenyutanchan <fenyutanchan@gmail.com>
2026-02-01 23:58:12 +0800

    switch to shell script for git commit backup

--- git_commit_backup.jl
+++
@@ -1,9 +0,0 @@
-using Dates
-
-timezone = round(now() - now(UTC), Minute(30))
-timezone_sign = sign(timezone)
-timezone = round(now(), Day(1)) + timezone_sign * timezone
-
-backup_time = Dates.format(now(), "yyyy-mm-dd HH:MM:SS") * " UTC" * (timezone_sign ≥ 0 ? "+" : "-") * Dates.format(timezone, "HHMM")
-
-run(`git commit -m "backup: $backup_time"`)
---
+++ git_commit_backup.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+git commit -m "backup: $(date +'%Y-%m-%d %H:%M:%S UTC%z')"
